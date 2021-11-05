f0311797a44ff60e51a1c6ef846d9c93ad2e6a9d
snailman07 <93746329+snailman07@users.noreply.github.com>
2021-11-05 12:30:19 +1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@ var snails = 0;
 var clickValue = 1;
 var autoClickers = 0;
 var scoreDisplay = document.getElementById("scoreDisplay");
+var lastClickDisplay = document.getElementById("lastClick");
 
 function displaySnails() {
 	scoreDisplay.innerHTML = snails;
-	console.log(snails)
+	lastClickDisplay.innerHTML = clickValue;
+	console.log(snails);
 }
 
 function addSnails() {
@@ -14,6 +16,7 @@ function addSnails() {
 }
 
 function addAutoCliker() {
+	snails -= 100;
 	autoClickers ++;
 	let toAdd = autoClickers * clickValue;
 	setInterval(function(){ 
@@ -23,5 +26,6 @@ function addAutoCliker() {
 }
 
 function increaseClickValue() {
+	snails -= 100;
 	clickValue ++;
 }
